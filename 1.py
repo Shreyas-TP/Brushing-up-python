afff61f72070946fd5f788477ba91ee59110c4a8
Shreyas TP <shreyastp8800@gmail.com>
2026-02-24 10:50:35 +0530

    basics

--- 1.py
+++ 1.py
@@ -6,3 +6,16 @@ b=20
 print (a+b)
 
 
+name = "Shreyas"
+age = 21
+height = 5.9
+is_student = True
+
+print(type(name)) #type checking
+
+
+age ="21"       #it is string now
+int_age = int(age)     #type casting/conversion
+print(int_age)    #type checking after type casting
+x=float(int_age)
+print(x)
