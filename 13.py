c3cc97a5dc4376ad321b36bafe374c7d2acfe8c6
Shreyas TP <shreyastp8800@gmail.com>
2026-02-28 23:12:39 +0530

    Functions - Advanced Concepts

--- 13.py
+++ 13.py
@@ -1,6 +1,21 @@
-#Lopps
-i=10
-while i<=50:
-    print(i)
-    i+=10
-   
+#lambda functions
+#lambda functions are anonymous functions that can have any number of arguments, 
+# but can only have one expression. They are often used for short, 
+# simple functions that are not reused elsewhere in the code.
+
+def total_sum(*numbers):
+    result = 0
+    for num in numbers:
+        result += num
+    return result
+
+print(total_sum(1, 2, 3, 4))
+
+#Recursion
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(5))
